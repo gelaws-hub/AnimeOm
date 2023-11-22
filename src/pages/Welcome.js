2f6ca23dc7f63cd325e9c4ad00d9e17140e80e9f
ibnu fadhil <fadhilgelaws@gmail.com>
2023-11-22 19:15:29 +0700

    changed the gif 2

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -5,7 +5,7 @@ export default function Welcome() {
         <>
         <div className="background-container"></div>
             <div className="containerWelcome">
-                <img src="https://i.pinimg.com/originals/40/95/a1/4095a1f95e16a96935140efdfcb38dc9.gif"></img>
+                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a58cd117-cad8-4640-bdd8-53116ac880cd/devyzsc-2d95f0e9-51c5-4cff-9859-e9ff821ef9ea.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E1OGNkMTE3LWNhZDgtNDY0MC1iZGQ4LTUzMTE2YWM4ODBjZFwvZGV2eXpzYy0yZDk1ZjBlOS01MWM1LTRjZmYtOTg1OS1lOWZmODIxZWY5ZWEuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.SrGmsoJ0G6UpMy6VW_wZe8I56hzYswauJmIiC_H0qrk"></img>
                 <h1 id="welcome">Hello!</h1>
                 <p id="welcomeMessage2">Welcome Anime lovers! I see that you are looking for an Anime</p>
                 <p id="welcomeMessage2">So, let's find one that suits your taste soon enough! </p>

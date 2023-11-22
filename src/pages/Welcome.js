181c14e5e006285ffa1af1eddfc04a3793e23a2c
ibnu fadhil <fadhilgelaws@gmail.com>
2023-11-22 19:10:57 +0700

    edited the gif on splash screen

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -5,7 +5,7 @@ export default function Welcome() {
         <>
         <div className="background-container"></div>
             <div className="containerWelcome">
-                <img src="https://media.tenor.com/Psq0TSQb_b0AAAAi/welcome-anime.gif"></img>
+                <img src="https://i.pinimg.com/originals/40/95/a1/4095a1f95e16a96935140efdfcb38dc9.gif"></img>
                 <h1 id="welcome">Hello!</h1>
                 <p id="welcomeMessage2">Welcome Anime lovers! I see that you are looking for an Anime</p>
                 <p id="welcomeMessage2">So, let's find one that suits your taste soon enough! </p>
